eca56b54755f22f12753841957c0f910713ded44
toilahiep01 <88882056+toilahiep01@users.noreply.github.com>
2021-08-13 16:32:35 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,17 @@
 // CONFIG DATA 
 const CONFIGDATA = {
-    titleHeader: 'Làm người yêu tớ nhé I Love You',
-    descriptionHeader: 'Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^',
-    buttonYes: 'Đồng Ý',
-    buttonNo: 'Không! Còn lâu nhé.',
-    titleModar: 'Cảm ơn em vì đồng ý',
-    descriptionModar: 'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    titleHeader: 'Thấy hiệp đẹp trai không',
+    descriptionHeader: 'Nếu bạn thoát và không trả lời câu hỏi thì bạn tự nhận tôi đẹp trai rồi nhé hehe ^^',
+    buttonYes: 'Đẹp trai',
+    buttonNo: 'Không! Bạn xấu vl.',
+    titleModar: 'Cảm ơn em vì đã khen tôi đẹp trai hehe',
+    descriptionModar: 'Tớ biết là bạn sẽ khen tôi đẹp trai mà ^^ </br> Cám ơn bạn vì đã khen tôi đẹp trai :)) </br> "Tôi biết tôi đẹp trai mà đừng khen nữa. </br> Ngại lắm hehe "',
 };
 
 
 
 
 
-// Author: andyngojs
-// Facebook: http://fb.com/dn279
-// Join us: https://fb.com/groups/dobeeteam.community
 
 
 
